08940474948ac8496f4405de70e9ddc2c355a7a6
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2020-01-08 08:42:32 +0100

    Updated bento and added centos-norcams-7.7

--- packer_templates/centos/scripts/fix-ifs.sh
+++ packer_templates/centos/scripts/fix-ifs.sh
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+# Override udev net-rules
+mkdir -p /etc/udev/rules.d/60-net.rules
+
+>>>>>>> Updated bento and added centos-norcams-7.7
 # Add"net.ifnames=0" and "biosdevname=0"
 sed -i -e 's/quiet/net.ifnames=0 biosdevname=0 quiet/' /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
