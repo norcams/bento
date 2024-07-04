80e7ab2f5ab286a979106c18bcfc643999392aef
Raymond Kristiansen <raymond.kristiansen@uib.no>
2024-07-04 07:57:27 +0200

    feat: added norcams el9 box

--- packer_templates/scripts/norcams/repo.sh
+++ packer_templates/scripts/norcams/repo.sh
@@ -2,8 +2,9 @@
 
 if command -v dnf >/dev/null 2>&1; then
 
+  rm -f /etc/yum.repos.d/*.repo
   repo_dist=$(uname -r | sed 's/.*\(el[0-9]\).*x86.*/\1/')
-  repo="https://download.iaas.uio.no/uh-iaas/prod/${repo_dist}"
+  repo="https://download.iaas.uio.no/nrec/prod/${repo_dist}"
 
   cat > /etc/yum.repos.d/almalinux.repo<<- EOM
 [base]
@@ -11,21 +12,21 @@ name=AlmaLinux-\$releasever - Base
 baseurl=$repo/almalinux-base/
 enabled=1
 gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux-${repo_dist:2}
 
 [appstream]
 name=AlmaLinux-\$releasever - AppStream
 baseurl=$repo/almalinux-AppStream/
 enabled=1
 gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux-${repo_dist:2}
 
 [extras]
 name=AlmaLinux-\$releasever - Extras
 baseurl=$repo/almalinux-extras/
 enabled=1
 gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux-${repo_dist:2}
 
 EOM
 
